53fe88cdcd4f420c0b1eb51143b32f2bf7f9f733
Harshiv Patel <pharshiv10@gmail.com>
2024-03-10 03:15:12 -0400

    Update settings.py for vercel deployment

--- chat/settings.py
+++ chat/settings.py
@@ -14,9 +14,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'e1vz(op2w&u_d-f(g(@crx!9&w390h!xyma2&)womo(*-dx%)!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
